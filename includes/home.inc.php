ad46263e52e47eddeed4c9483632192f8e70d039
Berg van den, Sam - Student Rijn IJssel <00311977@rijnijssel.nl>
2024-09-10 15:45:00 +0200

    home include

--- includes/home.inc.php
+++ includes/home.inc.php
@@ -1,5 +1,3 @@
-<!-- set title so the index.php can
-
-<div class="bg-red-500">
-    <h1 class="text-white text-4xl text-center">Welcome to DigiDate</h1>
-</div>
+<?php
+// include the navbar
+include("./partials/navbar.inc.php");
