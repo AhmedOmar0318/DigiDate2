9a180ea48e7d210fd5c51f6e646c03b1d6cd8daf
Büyükleyla, Yilmaz - Student Rijn IJssel <00312238@rijnijssel.nl>
2024-09-17 10:49:49 +0200

    aangepast

--- php/view_admin.php
+++ php/view_admin.php
@@ -7,7 +7,8 @@
 if (!isset($_SESSION['userId']) || $_SESSION['role'] !== 2) {
     exit('Access denied.');
 }
-test
+include '../private/conn_digidate_examen.php';
+
 try {
     // Prepare the query to fetch users with roleId = 2 (Admins)
     $query = "SELECT firstName, middleName, lastName, email FROM users WHERE roleId = 2 and activated = 1";
